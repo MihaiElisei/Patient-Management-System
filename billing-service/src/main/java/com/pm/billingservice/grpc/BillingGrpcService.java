2211d89db78cbab7213904f88548ac68a68ea928
Mihai <mihai.m.elisei@gmail.com>
2026-02-05 17:00:45 +0200

    implemented grpc server

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import billing.BillingRequest;
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends  BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received : {}", billingRequest.toString());
+
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("12344")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
+}
